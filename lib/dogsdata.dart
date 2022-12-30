d8476a1d96f7fd2150447b32a514fec12937a0c0
riskiilyas <riskiilyas03@gmail.com>
2022-12-30 14:07:14 +0700

    Dogs data in progress

---
+++ lib/dogsdata.dart
@@ -0,0 +1,115 @@
+import 'package:flutter/material.dart';
+
+class Dog {
+  final String name;
+  final String desc;
+  final Image image;
+
+  Dog({required this.name, required this.desc, required this.image});
+}
+
+final DOGS_DATA = [
+  Dog(
+      name: 'German Shepherd',
+      image: Image.network(
+          'https://cdn.britannica.com/79/232779-050-6B0411D7/German-Shepherd-dog-Alsatian.jpg'),
+      desc: 'The German Shepherd or Alsatian is a German breed of working dog of medium to large size. The breed was developed by Max von Stephanitz using various traditional German herding dogs from 1899. It was originally bred as a herding dog, for herding sheep.'),
+  Dog(
+    name: '',
+    image: Image.network(
+    ''
+    ),
+    desc: ''
+  ),
+  Dog(
+      name: '',
+      image: Image.network(
+          ''
+      ),
+      desc: ''
+  ),
+  Dog(
+      name: '',
+      image: Image.network(
+          ''
+      ),
+      desc: ''
+  ),
+  Dog(
+      name: '',
+      image: Image.network(
+          ''
+      ),
+      desc: ''
+  ),
+  Dog(
+      name: '',
+      image: Image.network(
+          ''
+      ),
+      desc: ''
+  ),
+  Dog(
+      name: '',
+      image: Image.network(
+          ''
+      ),
+      desc: ''
+  ),
+  Dog(
+      name: '',
+      image: Image.network(
+          ''
+      ),
+      desc: ''
+  ),
+  Dog(
+      name: '',
+      image: Image.network(
+          ''
+      ),
+      desc: ''
+  ),
+  Dog(
+      name: '',
+      image: Image.network(
+          ''
+      ),
+      desc: ''
+  ),
+  Dog(
+      name: '',
+      image: Image.network(
+          ''
+      ),
+      desc: ''
+  ),
+  Dog(
+      name: '',
+      image: Image.network(
+          ''
+      ),
+      desc: ''
+  ),
+  Dog(
+      name: '',
+      image: Image.network(
+          ''
+      ),
+      desc: ''
+  ),
+  Dog(
+      name: '',
+      image: Image.network(
+          ''
+      ),
+      desc: ''
+  ),
+  Dog(
+      name: '',
+      image: Image.network(
+          ''
+      ),
+      desc: ''
+  ),
+];
--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Doggopedia',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -24,7 +24,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Doggopedia'),
     );
   }
 }
@@ -48,18 +48,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
 
   @override
   Widget build(BuildContext context) {
@@ -74,6 +62,10 @@ class _MyHomePageState extends State<MyHomePage> {
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
+        automaticallyImplyLeading: false,
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.search))
+        ],
       ),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
@@ -95,21 +87,9 @@ class _MyHomePageState extends State<MyHomePage> {
           // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
